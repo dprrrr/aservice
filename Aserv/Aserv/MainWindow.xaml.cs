404547a4e09f210d8430a7ec9b7a864dbb0cddce
nikitaIZERGIN <izergin-nikitka@mail.ru>
2021-02-19 10:15:59 +0300

    сделала всё, до добавления и редактирования услуг

--- Aserv/Aserv/MainWindow.xaml.cs
+++ Aserv/Aserv/MainWindow.xaml.cs
@@ -65,6 +65,20 @@ namespace Aserv
                 return HasDiscount ? "None" : "Strikethrough";
             }
         }
+        public float DiscountFloat
+        {
+            get
+            {
+                return Convert.ToSingle(Discount ?? 0);
+            }
+        }
+        public string DescriptionString
+        {
+            get
+            {
+                return Description ?? "";
+            }
+        }
     }
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
@@ -86,7 +100,22 @@ namespace Aserv
         private List<Service> _ServiceList;
         public List<Service> ServiceList
         {
-            get { return _ServiceList; }
+
+            get
+            {
+                var FilteredServiceList = _ServiceList.FindAll(item =>
+    item.DiscountFloat >= CurrentDiscountFilter.Item1 &&
+    item.DiscountFloat < CurrentDiscountFilter.Item2);
+                if (SearchFilter != "")
+                    FilteredServiceList = FilteredServiceList.Where(item =>
+                        item.Title.IndexOf(SearchFilter, StringComparison.OrdinalIgnoreCase) != -1 ||
+                        item.DescriptionString.IndexOf(SearchFilter, StringComparison.OrdinalIgnoreCase) != -1).ToList();
+                if (SortPriceAscending)
+
+                   return FilteredServiceList.OrderBy(item => Double.Parse(item.CostWithDiscount)).ToList();
+                else
+                   return FilteredServiceList.OrderByDescending(item => Double.Parse(item.CostWithDiscount)).ToList();
+            }
             set { _ServiceList = value; }
         }
 
@@ -152,12 +181,116 @@ namespace Aserv
                 if (PropertyChanged != null)
                 {
                     PropertyChanged(this, new PropertyChangedEventArgs("ServiceList"));
+                    PropertyChanged(this, new PropertyChangedEventArgs("ServicesCount"));
+                    PropertyChanged(this, new PropertyChangedEventArgs("FilteredServicesCount"));
                 }
+            }
+        }
+        private string _SearchFilter = "";
+         public string SearchFilter
+        {
+            get { return _SearchFilter; }
+            set
+            {
+                _SearchFilter = value;
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs("ServiceList"));
+                    PropertyChanged(this, new PropertyChangedEventArgs("ServicesCount"));
+                    PropertyChanged(this, new PropertyChangedEventArgs("FilteredServicesCount"));
+                }
+
             }
         }
         private void RadioButton_Checked(object sender, RoutedEventArgs e)
         {
             SortPriceAscending = (sender as RadioButton).Tag.ToString() == "1";
         }
+        private List<Tuple<string, double, double>> FilterByDiscountValuesList =
+            new List<Tuple<string, double, double>>()
+            {
+        Tuple.Create("Все записи", 0d, 1d),
+        Tuple.Create("от 0% до 5%", 0d, 0.05d),
+        Tuple.Create("от 5% до 15%", 0.05d, 0.15d),
+        Tuple.Create("от 15% до 30%", 0.15d, 0.3d),
+        Tuple.Create("от 30% до 70%", 0.3d, 0.7d),
+        Tuple.Create("от 70% до 100%", 0.7d, 1d)
+    };
+
+        public List<string> FilterByDiscountNamesList
+        {
+            get
+            {
+                return FilterByDiscountValuesList
+                    .Select(item => item.Item1)
+                    .ToList();
+            }
+        }
+        private Tuple<double, double> _CurrentDiscountFilter = Tuple.Create(double.MinValue, double.MaxValue);
+
+        public Tuple<double, double> CurrentDiscountFilter
+        {
+            get
+            {
+                return _CurrentDiscountFilter;
+            }
+            set
+            {
+                _CurrentDiscountFilter = value;
+                if (PropertyChanged != null)
+                {
+                    // при изменении фильтра список перерисовывается
+                    PropertyChanged(this, new PropertyChangedEventArgs("ServiceList"));
+                }
+            }
+
+        }
+        private void DiscountFilterComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            CurrentDiscountFilter = Tuple.Create(
+                FilterByDiscountValuesList[DiscountFilterComboBox.SelectedIndex].Item2,
+                FilterByDiscountValuesList[DiscountFilterComboBox.SelectedIndex].Item3
+            );
+        }
+
+        public int ServicesCount
+        {
+            get
+            {
+                return _ServiceList.Count;
+            }
+
+        }
+
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            // у DataGrid-a есть свойство SelectedItem - его приводим к типу Service
+            var item = MainDataGrid.SelectedItem as Service;
+
+            // по условиям задачи нельзя удалять только те услуги, которые уже оказаны
+            // свойство ClientService ссылается на таблицу оказанных услуг
+            if (item.ClientService.Count > 0)
+            {
+                MessageBox.Show("Нельзя удалять услугу, она уже оказана");
+                return;
+            }
+
+            // метод Remove нужно завернуть в конструкцию try..catch, на случай, если 
+            // база спроектирована криво и нет каскадного удаления - это сделайте сами
+            Core.DB.Service.Remove(item);
+
+            // сохраняем изменения
+            Core.DB.SaveChanges();
+
+            // перечитываем изменившийся список, не забывая в сеттере вызвать PropertyChanged
+            ServiceList = Core.DB.Service.ToList();
+        }
+
+        private void TextBox_KeyUp(object sender, KeyEventArgs e)
+        {
+            SearchFilter = SearchFilterTextBox.Text;
+        }
+
     }
 }
