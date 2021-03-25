04fea9cc4802717711371294e996391b31cc58ac
Dprrrr <dprrrr1987@mail.ru>
2021-03-25 10:48:45 +0300

    com

---
+++ Aserv/Core.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aserv
+{
+    class Core
+    {
+        // demoEntities это название подключения, которое вы дали при создании модели
+        public static knemetzhanovaEntities DB = new knemetzhanovaEntities();
+    }
+}
